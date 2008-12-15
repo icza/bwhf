e192cbcfb5de6f05437a4663ac93d22093c16bef
iczaaa <iczaaa@75045378-b607-11dd-893d-d98698f66bbf>
2008-12-15 08:44:55 +0000

    Hacker list control section done (filtering, sorting).

--- trunk/hackerdb/src/hu/belicza/andras/hackerdb/ApiConsts.java
+++ trunk/hackerdb/src/hu/belicza/andras/hackerdb/ApiConsts.java
@@ -26,4 +26,28 @@ public class ApiConsts {
 	/** String representations of gateways. Only the index should be sent over. */
 	public static final String[] GATEWAYS = new String[] { "USEast", "USWest", "Europe", "Asia", "iCCup", "Other" };
 	
+	/** Filter name for name.             */
+	public static final String FILTER_NAME_NAME               = "nam";
+	/** Filter name for no all gateways.  */
+	public static final String FILTER_NAME_NO_ALL_GATEWAYS    = "nag";
+	/** Filter name for gateway.          */
+	public static final String FILTER_NAME_GATEWAY            = "gat";
+	/** Filter name for min report count. */
+	public static final String FILTER_NAME_MIN_REPORT_COUNT   = "mrc";
+	/** Filter name for page.             */
+	public static final String FILTER_NAME_PAGE               = "pag";
+	/** Filter name for page size.        */
+	public static final String FILTER_NAME_PAGE_SIZE          = "pgs";
+	/** Filter name for sort by.          */
+	public static final String FILTER_NAME_SORT_BY            = "sby";
+	/** Filter name for sort by.          */
+	public static final String FILTER_NAME_DESCENDANT_SORTING = "dst";
+	
+	/** Sort by value for name.           */
+	public static final String SORT_BY_VALUE_NAME             = "snm";
+	/** Sort by value for gateway.           */
+	public static final String SORT_BY_VALUE_GATEWAY          = "sgt";
+	/** Sort by value for report count.   */
+	public static final String SORT_BY_VALUE_REPORT_COUNT     = "src";
+	
 }
--- trunk/hackerdb/src/hu/belicza/andras/hackerdb/HackerDbServlet.java
+++ trunk/hackerdb/src/hu/belicza/andras/hackerdb/HackerDbServlet.java
@@ -1,5 +1,7 @@
 package hu.belicza.andras.hackerdb;
 
+import static hu.belicza.andras.hackerdb.ApiConsts.*;
+
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.Connection;
@@ -13,8 +15,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.dbcp.BasicDataSource;
-
-import static hu.belicza.andras.hackerdb.ApiConsts.*;
 
 /**
  * Servlet to serve hacker list related requests.<br>
@@ -41,18 +41,24 @@ public class HackerDbServlet extends HttpServlet {
 		dataSource.setUrl( DATABASE_URL );
 	}
 	
+	@Override
+	public void doPost( final HttpServletRequest request, final HttpServletResponse response ) {
+		doGet( request, response );
+	}
 	
 	@Override
 	public void doGet( final HttpServletRequest request, final HttpServletResponse response ) {
 		setNoCache( response );
 		
 		try {
-			final String operation = request.getParameter( REQUEST_PARAMETER_NAME_OPERATION );
+			String operation = request.getParameter( REQUEST_PARAMETER_NAME_OPERATION );
 			
 			if ( operation == null )
-				throw new BadRequestException();
+				operation = OPERATION_LIST;
 			
 			if ( operation.equals( OPERATION_LIST ) ) {
+				
+				serveHackerList( request, response );
 				
 			} else if ( operation.equals( OPERATION_CHECK ) ) {
 				
@@ -64,7 +70,6 @@ public class HackerDbServlet extends HttpServlet {
 				sendBackPlainMessage( Boolean.toString( checkKey( key ) ), response );
 				
 			} else if ( operation.equals( OPERATION_REPORT ) ) {
-				// Check the validity of an authorization key
 				
 				final String key = request.getParameter( REQUEST_PARAMETER_NAME_KEY );
 				if ( key == null )
@@ -77,7 +82,7 @@ public class HackerDbServlet extends HttpServlet {
 				catch ( final Exception e ) {
 					throw new BadRequestException();
 				}
-				if ( gatewayIndex < 0 || gatewayIndex >= ApiConsts.GATEWAYS.length )
+				if ( gatewayIndex < 0 || gatewayIndex >= GATEWAYS.length )
 					throw new BadRequestException();
 				
 				final String[] playerNames = new String[ 8 ];
@@ -91,6 +96,108 @@ public class HackerDbServlet extends HttpServlet {
 		}
 		catch ( final BadRequestException bre ) {
 			sendBackErrorMessage( response );
+		}
+	}
+	
+	/**
+	 * Serves a part of the hacker list.
+	 * @param request  the http request
+	 * @param response the http response
+	 */
+	private void serveHackerList( final HttpServletRequest request, final HttpServletResponse response ) {
+		setNoCache( response );
+		response.setContentType( "text/html" );
+		
+		PrintWriter outputWriter = null;
+		try {
+			outputWriter = response.getWriter();
+			
+			outputWriter.println( "<html><head><title>BWHF Hacker data base</title></head><body><center>" );
+			// Header section
+			outputWriter.println( "<h2>BWHF Hacker data base</h2>" );
+			outputWriter.println( "<p align=right>Go to <a href='http://code.google.com/p/bwhf'>BWHF Agent home page</a></p>" );
+			
+			// Controls section
+			outputWriter.println( "<p>" );
+			outputWriter.println( "<form action='/hackers' method='GET'>" );
+			
+			// Filters
+			outputWriter.println( "<b>Filters:</b><table border=1>" );
+			outputWriter.println( "<tr><th>Name<th>Gateway<th>Min. report count" );
+			outputWriter.println( "<tr><td><input name='" + FILTER_NAME_NAME + "' type=text value='" + getStringParamValue( request, FILTER_NAME_NAME ) + "'><td>" );
+			
+			// Render gateways here
+			outputWriter.println( "<input type=hidden name='" + FILTER_NAME_NO_ALL_GATEWAYS + "' value='no'>" ); // First we select all gatways, on subsequent calls we only want to select selected gateways
+			for ( int i = 0; i < GATEWAYS.length; i++ )
+				outputWriter.println( "<input name='" + FILTER_NAME_GATEWAY + i + "' type=checkbox " + ( request.getParameter( FILTER_NAME_NO_ALL_GATEWAYS ) == null || request.getParameter( FILTER_NAME_GATEWAY + i ) != null ? "checked" : "" ) + ">" + GATEWAYS[ i ] );
+			outputWriter.println( "<td><input name='" + FILTER_NAME_MIN_REPORT_COUNT +"' type=text value='" + getIntParamValue( request, FILTER_NAME_MIN_REPORT_COUNT, 1 ) + "'>" );
+			outputWriter.println( "</table>" );
+			outputWriter.println( "<br><b>Hackers matching the filters: 45</b>" );
+			
+			// Sorting
+			outputWriter.println( "<br>Sort hackers by: <select name='" + FILTER_NAME_SORT_BY + "'>" );
+			final String sortByValue = getStringParamValue( request, FILTER_NAME_SORT_BY );
+			outputWriter.println( "<option value='" + SORT_BY_VALUE_NAME + "' " + ( sortByValue.equals( SORT_BY_VALUE_NAME ) ? "selected" : "" ) + ">Name</value>" );
+			outputWriter.println( "<option value='" + SORT_BY_VALUE_GATEWAY + "' " + ( sortByValue.equals( SORT_BY_VALUE_GATEWAY ) ? "selected" : "" ) + ">Gateway</value>" );
+			outputWriter.println( "<option value='" + SORT_BY_VALUE_REPORT_COUNT + "' " + ( sortByValue.equals( SORT_BY_VALUE_REPORT_COUNT ) ? "selected" : "" ) + ">Report count</value>" );
+			outputWriter.println( "</select>" );
+			final boolean descendantSorting = request.getParameter( FILTER_NAME_DESCENDANT_SORTING ) != null ;
+			outputWriter.println( "<input name='" + FILTER_NAME_DESCENDANT_SORTING + "' type=checkbox " + ( descendantSorting ? "checked" : "" ) + ">Descendant sorting" );
+			
+			// Pagination
+			outputWriter.println( "<br><input type=submit value='First'>" );
+			outputWriter.println( "<input type=submit value='Previous'>" );
+			outputWriter.println( "Page <input name='" + FILTER_NAME_PAGE + "' type=text value='" + getIntParamValue( request, FILTER_NAME_PAGE, 1 ) + "' size=1> out of <b>10</b>, page size:" );
+			outputWriter.println( "<input name='" + FILTER_NAME_PAGE_SIZE + "' type=text value='" + getIntParamValue( request, FILTER_NAME_PAGE_SIZE, 20 ) + "' size=1>" );
+			outputWriter.println( "<input type=submit value='Next'>" );
+			outputWriter.println( "<input type=submit value='Last'>" );
+			outputWriter.println( "</form></p>" );
+			
+			// Hackers table section
+			outputWriter.println( "<table border=1>" );
+			outputWriter.println( "<tr><th>#<th>Name" + ( sortByValue.equals( SORT_BY_VALUE_NAME ) ? ( descendantSorting ? " &darr;" : " &uarr;" ) : "" )
+					+ "<th>Gateway" + ( sortByValue.equals( SORT_BY_VALUE_GATEWAY ) ? ( descendantSorting ? " &darr;" : " &uarr;" ) : "" )
+					+ "<th>Report count" + ( sortByValue.equals( SORT_BY_VALUE_REPORT_COUNT ) ? ( descendantSorting ? " &darr;" : " &uarr;" ) : "" ) + "" );
+			outputWriter.println( "</table>" );
+			
+			// Footer section
+			outputWriter.println( "<p align=right><i>&copy; Andr&aacute;s Belicza, 2008</i></p>" );
+			outputWriter.println( "</center></body></html>" );
+			
+			outputWriter.flush();
+		} catch ( final IOException ie ) {
+			ie.printStackTrace();
+		}
+		finally {
+			if ( outputWriter != null )
+				outputWriter.close();
+		}
+	}
+	
+	/**
+	 * Returns the value of a request parameter.
+	 * @param request   http request object
+	 * @param paramName name of the parameter whose value is to be returned
+	 * @return the value of a request parameter or an empty string if the parameter does not have a value
+	 */
+	private String getStringParamValue( final HttpServletRequest request, final String paramName ) {
+		final String paramValue = request.getParameter( paramName );
+		return paramValue == null ? "" : paramValue;
+	}
+	
+	/**
+	 * Returns the integer value parsed from a request parameter.
+	 * @param request   http request object
+	 * @param paramName name of the parameter whose value is to be returned
+	 * @param defaultValue
+	 * @return the value of a request parameter or <code>defaultValue</code> if the parameter does not have a value or it does not contain a valid integer
+	 */
+	private int getIntParamValue( final HttpServletRequest request, final String paramName, final int defaultValue ) {
+		try {
+			return Integer.parseInt( request.getParameter( paramName ) );
+		}
+		catch ( final Exception e ) {
+			return defaultValue;
 		}
 	}
 	
