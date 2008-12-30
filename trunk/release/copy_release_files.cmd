cc7aa279f1c380df83dbd2f54c96c13ebe6faeb7
iczaaa <iczaaa@75045378-b607-11dd-893d-d98698f66bbf>
2008-12-30 13:25:04 +0000

    Added a release assembler script, and added link to version history wiki page in the about html template.

---
+++ trunk/release/copy_release_files.cmd
@@ -0,0 +1,25 @@
+@echo off
+
+%~d0
+cd "%~dp0"
+
+SET RELEASE_DIR="T:\BWHFAgent"
+
+IF NOT EXIST %RELEASE_DIR% md %RELEASE_DIR%
+
+IF EXIST %RELEASE_DIR% goto copyfiles
+echo Cannot create directory: %RELEASE_DIR%!
+pause
+goto quit
+
+:copyfiles
+xcopy /Q BWHFAgent.cmd %RELEASE_DIR%
+xcopy /Q BWHFAgent-console.cmd %RELEASE_DIR%
+xcopy /E /Q ..\bin\hu %RELEASE_DIR%\BWHFAgent.jar\hu\
+xcopy /Q ..\hackerdb\webapp-root\WEB-INF\classes\hu\belicza\andras\hackerdb\ServerApiConsts.class %RELEASE_DIR%\BWHFAgent.jar\hu\belicza\andras\hackerdb\
+xcopy /Q ..\sounds\*.* %RELEASE_DIR%\sounds\
+xcopy /Q ..\lib\swingwt.jar %RELEASE_DIR%\lib\
+xcopy /Q ..\lib\win32\*.* %RELEASE_DIR%\lib\win32\
+xcopy /Q META-INF\MANIFEST.MF %RELEASE_DIR%\BWHFAgent.jar\META-INF\
+
+:quit
