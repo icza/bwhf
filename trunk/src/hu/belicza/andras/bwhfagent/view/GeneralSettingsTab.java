afc20c1605f2a7c62a4997f92ac39c5da0393232
iczaaa <iczaaa@75045378-b607-11dd-893d-d98698f66bbf>
2009-01-12 07:45:26 +0000

    Added sound volume control property and UI elements.

--- trunk/src/hu/belicza/andras/bwhfagent/view/GeneralSettingsTab.java
+++ trunk/src/hu/belicza/andras/bwhfagent/view/GeneralSettingsTab.java
@@ -5,12 +5,15 @@ import hu.belicza.andras.bwhfagent.Consts;
 import java.net.URL;
 
 import swingwt.awt.BorderLayout;
+import swingwt.awt.Dimension;
 import swingwt.awt.event.ActionEvent;
 import swingwt.awt.event.ActionListener;
 import swingwtx.swing.JButton;
 import swingwtx.swing.JCheckBox;
+import swingwtx.swing.JLabel;
 import swingwtx.swing.JOptionPane;
 import swingwtx.swing.JPanel;
+import swingwtx.swing.JSlider;
 
 /**
  * General settings tab.
@@ -28,6 +31,8 @@ public class GeneralSettingsTab extends Tab {
 	private final JButton   checkUpdatesButton                 = new JButton( CHECK_UPDATES_BUTTON_TEXT );
 	/** Checkbox to tell whether check for updates automatically on startup. */
 	public  final JCheckBox skipLatterActionsOfHackersCheckBox = new JCheckBox( "During a replay scan if a player is found hacking, skip scanning his latter actions", Boolean.parseBoolean( Utils.settingsProperties.getProperty( Consts.PROPERTY_SKIP_LATTER_ACTIONS_OF_HACKERS ) ) );
+	/** Slider to set the sound volume.                                      */
+	public  final JSlider   soundVolumeSlider                  = new JSlider( 0, 100, Integer.parseInt( Utils.settingsProperties.getProperty( Consts.PROPERTY_SOUND_VOLUME ) ) );
 	
 	/**
 	 * Creates a new GeneralSettings.
@@ -45,7 +50,7 @@ public class GeneralSettingsTab extends Tab {
 	 * Builds the GUI of the tab.
 	 */
 	private void buildGUI() {
-		final JPanel panel = new JPanel();
+		JPanel panel = new JPanel();
 		panel.add( checkUpdatesOnStartupCheckBox );
 		// In SwingWT Button text cannot be changed if a mnemonic has been assigned
 		//checkUpdatesButton.setMnemonic( ( checkUpdatesButton.getText().charAt( 0 ) ) );
@@ -58,8 +63,17 @@ public class GeneralSettingsTab extends Tab {
 		contentBox.add( panel );
 		
 		contentBox.add( Utils.wrapInPanel( skipLatterActionsOfHackersCheckBox ) );
+		
+		panel = new JPanel(); 
+		panel.add( new JLabel( "Sound volume:" ) );
+		soundVolumeSlider.setLabelTable( soundVolumeSlider.createStandardLabels( 10 ) );
+		soundVolumeSlider.setPreferredSize( new Dimension( 300, 45 ) );
+		panel.add( soundVolumeSlider );
+		contentBox.add( panel );
+		
 		// To consume the remaining space:
 		contentBox.add( new JPanel( new BorderLayout() ) );
+		
 	}
 	
 	/**
@@ -100,6 +114,7 @@ public class GeneralSettingsTab extends Tab {
 	public void assignUsedProperties() {
 		Utils.settingsProperties.setProperty( Consts.PROPERTY_CHECK_UPDATES_ON_STARTUP      , Boolean.toString( checkUpdatesOnStartupCheckBox.isSelected() ) );
 		Utils.settingsProperties.setProperty( Consts.PROPERTY_SKIP_LATTER_ACTIONS_OF_HACKERS, Boolean.toString( skipLatterActionsOfHackersCheckBox.isSelected() ) );
+		Utils.settingsProperties.setProperty( Consts.PROPERTY_SOUND_VOLUME                  , Integer.toString( soundVolumeSlider.getValue() ) );
 	}
 	
 }
