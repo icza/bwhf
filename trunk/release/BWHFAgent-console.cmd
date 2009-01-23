d9ff8f9a41d5a8a63e9d3bbed74ed487f911a4ab
iczaaa <iczaaa@75045378-b607-11dd-893d-d98698f66bbf>
2009-01-23 06:34:43 +0000

    Moved BWHFAgent.jar to the lib folder to avoid launching misunderstanding.

--- trunk/release/BWHFAgent-console.cmd
+++ trunk/release/BWHFAgent-console.cmd
@@ -1,1 +1,1 @@
-java -Djava.library.path=lib/win32 -cp BWHFAgent.jar;lib/swingwt.jar;lib/win32/swt.jar hu/belicza/andras/bwhfagent/BWHFAgent
+java -Djava.library.path=lib/win32 -cp lib/BWHFAgent.jar;lib/swingwt.jar;lib/win32/swt.jar hu/belicza/andras/bwhfagent/BWHFAgent
