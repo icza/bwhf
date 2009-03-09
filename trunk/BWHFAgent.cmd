4848702aa8d0af7bcaf069bf68f4414487ef2cb0
iczaaa <iczaaa@75045378-b607-11dd-893d-d98698f66bbf>
2009-03-09 15:10:27 +0000

    A replay passed to BWHF Agent will be opened in the charts tab (left out from previous commit).

--- trunk/BWHFAgent.cmd
+++ trunk/BWHFAgent.cmd
@@ -1,3 +1,3 @@
 @%~d0
 @cd "%~dp0"
-java -Djava.library.path=lib/win32 -cp bin;hackerdb/webapp-root/WEB-INF/classes;lib/pcx.jar;lib/swingwt.jar;lib/win32/swt.jar hu/belicza/andras/bwhfagent/BWHFAgent %1
+java -Djava.library.path=lib/win32 -cp bin;hackerdb/webapp-root/WEB-INF/classes;lib/pcx.jar;lib/swingwt.jar;lib/win32/swt.jar hu/belicza/andras/bwhfagent/BWHFAgent "%~f1"
