f27a232eee5cb11b9f23ff61d39303699c8b638d
iczaaa <iczaaa@75045378-b607-11dd-893d-d98698f66bbf>
2009-06-12 07:06:35 +0000

    Create tables script for the upcoming new Players' Network.

--- trunk/hackerdb/create_dbase_schema.sql
+++ trunk/hackerdb/create_dbase_schema.sql
@@ -1,3 +1,8 @@
+
+----------------------------------------
+---- Tables of the hacker registry -----
+----------------------------------------
+
 DROP TABLE IF EXISTS report;
 DROP TABLE IF EXISTS hacker;
 DROP TABLE IF EXISTS key;
@@ -54,5 +59,64 @@ CREATE CACHED TABLE download_log (
     success      BOOLEAN,
     exec_time_ms INT,                    --Execution time in ms.
     version      TIMESTAMP DEFAULT NOW
+);
+
+
+-----------------------------------------
+---- Tables of the Players' Network -----
+-----------------------------------------
+
+DROP TABLE IF EXISTS player_aka;
+DROP TABLE IF EXISTS game_player;
+DROP TABLE IF EXISTS player;
+DROP TABLE IF EXISTS game;
+
+CREATE CACHED TABLE game (
+    id            IDENTITY,
+    engine        INT,
+    frames        INT,
+    save_time     TIMESTAMP,
+    name          VARCHAR,
+    map_width     INT,
+    map_height    INT,
+    speed         INT,
+    type          INT,
+    sub_type      INT,
+    creator_name  VARCHAR,
+    map_name      VARCHAR,
+    replay_md5    VARCHAR,
+    agent_version VARCHAR,
+    ip            VARCHAR,               --IP of the reporter's computer.
+    version       TIMESTAMP DEFAULT NOW
+);
+
+
+CREATE CACHED TABLE player (
+    id      IDENTITY,
+    name    VARCHAR,
+    version TIMESTAMP DEFAULT NOW
+);
+
+
+CREATE CACHED TABLE game_player (
+    id            IDENTITY,
+    game          INT,
+    player        INT,
+    race          INT,
+    actions_count INT,
+    color         INT,
+    version       TIMESTAMP DEFAULT NOW,
+    FOREIGN KEY (game) REFERENCES game(id) ON DELETE CASCADE,
+    FOREIGN KEY (player) REFERENCES player(id) ON DELETE CASCADE
+);
+
+
+CREATE CACHED TABLE player_aka (
+    id         IDENTITY,
+    player     INT,
+    player_aka INT,
+    version    TIMESTAMP DEFAULT NOW,
+    FOREIGN KEY (player) REFERENCES player(id) ON DELETE CASCADE,
+    FOREIGN KEY (player_aka) REFERENCES player(id) ON DELETE CASCADE
 );
 
