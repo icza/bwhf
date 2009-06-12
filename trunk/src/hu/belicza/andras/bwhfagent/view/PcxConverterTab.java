4a4e66830cfc2f4fafc821717a48daf58842d399
iczaaa <iczaaa@75045378-b607-11dd-893d-d98698f66bbf>
2009-06-12 07:14:16 +0000

    Decreased time between checks for new pcx screenshots.

--- trunk/src/hu/belicza/andras/bwhfagent/view/PcxConverterTab.java
+++ trunk/src/hu/belicza/andras/bwhfagent/view/PcxConverterTab.java
@@ -33,7 +33,7 @@ public class PcxConverterTab extends ProgressLoggedTab {
 	/** PCX file extension.                            */
 	private static final String PCX_FILE_EXTENSION                       = ".pcx";
 	/** Time between checking for new PCX files in ms. */
-	private static final long   TIME_BETWEEN_CHECKS_FOR_NEW_PCX_FILES_MS = 1500l;
+	private static final long   TIME_BETWEEN_CHECKS_FOR_NEW_PCX_FILES_MS = 1000l;
 	
 	/** PCX file filter. */
 	private static final FileFilter SWING_PCX_FILE_FILTER = new FileFilter() {
