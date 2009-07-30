230191c4771001eff43e2d27655cc0cfba7f2265
iczaaa <icza@users.noreply.github.com>
2009-07-30 12:44:14 +0000

    Algorihtm to determine authoritativeness extent.

--- src/hu/belicza/andras/bwhfagent/view/AkaFinderTab.java
+++ src/hu/belicza/andras/bwhfagent/view/AkaFinderTab.java
@@ -8,8 +8,8 @@ import hu.belicza.andras.bwhfagent.Consts;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
-import java.util.Date;
 import java.util.List;
 
 import swingwt.awt.GridLayout;
@@ -36,7 +36,7 @@ public class AkaFinderTab extends ProgressLoggedTab {
 	/** Flag hacker reps checkbox.                       */
 	private final JCheckBox dontCompareSameNamesCheckBox = new JCheckBox( "Don't compare players with same names", Boolean.parseBoolean( Utils.settingsProperties.getProperty( Consts.PROPERTY_DONT_COMPARE_SAME_NAMES ) ) );
 	/** Authoritativeness threshold combobox.            */
-	private final JComboBox  authoritativenessThresholdComboBox   = new JComboBox( AuthoritativenessExtent.values() );
+	private final JComboBox  authoritativenessThresholdComboBox;
 	/** Matching probability treshold combobox.          */
 	private final JComboBox  matchingProbabilityThresholdComboBox = new JComboBox( new Object[] { "100%", "90%", "80%", "70%", "60%", "50%", "40%", "30%", "20%", "10%", "0%" } );
 	
@@ -56,18 +56,20 @@ public class AkaFinderTab extends ProgressLoggedTab {
 	 * @author Andras Belicza
 	 */
 	private static enum AuthoritativenessExtent {
-		EXCELLENT( "Excellent" ),
-		VERY_GOOD( "Very good" ),
-		GOOD     ( "Good"      ),
-		AVERAGE  ( "Average"   ),
-		POOR     ( "Poor"      ),
+		USELESS  ( "Useless"   ),
 		VERY_POOR( "Very poor" ),
-		USELESS  ( "Useless"   );
+		POOR     ( "Poor"      ),
+		AVERAGE  ( "Average"   ),
+		GOOD     ( "Good"      ),
+		VERY_GOOD( "Very good" ),
+		EXCELLENT( "Excellent" );
 		
 		private final String displayName; 
+		
 		private AuthoritativenessExtent( final String displayName ) {
 			this.displayName = displayName;
 		}
+		
 		@Override
 		public String toString() {
 			return displayName;
@@ -80,12 +82,13 @@ public class AkaFinderTab extends ProgressLoggedTab {
 	 */
 	private static class PlayerAnalysis {
 		final File   replay;
-		final Date   replayDate;
+		final long   replayDate;
 		final String playerName;
 		final byte   race;
 		final int    frames;
 		final int    realApm;
-		public PlayerAnalysis( final File replay, final Date replayDate, final String playerName, final byte race, final int frames, final int realApm ) {
+		
+		public PlayerAnalysis( final File replay, final long replayDate, final String playerName, final byte race, final int frames, final int realApm ) {
 			this.replay     = replay;
 			this.replayDate = replayDate;
 			this.playerName = playerName;
@@ -102,22 +105,67 @@ public class AkaFinderTab extends ProgressLoggedTab {
 	private static class Comparision implements Comparable< Comparision > {
 		final PlayerAnalysis          analysis1;
 		final PlayerAnalysis          analysis2;
-		final float                   matchingProbability;
 		final AuthoritativenessExtent authoritativenessExtent;
-		public Comparision( final PlayerAnalysis analysis1, final PlayerAnalysis analysis2 ) {
+		final float                   matchingProbability;
+		final boolean                 tresholdReached;
+		
+		public Comparision( final PlayerAnalysis analysis1, final PlayerAnalysis analysis2, final AuthoritativenessExtent authoritativenessExtentTreshold, final float matchingProbabilityTreshold ) {
 			this.analysis1 = analysis1;
 			this.analysis2 = analysis2;
-			this.matchingProbability = (float) Math.random() * 100.0f;
-			this.authoritativenessExtent = AuthoritativenessExtent.values()[ (int) ( Math.random() * AuthoritativenessExtent.values().length ) ];
+			authoritativenessExtent = determineAuthoritativenessExtent();
+			// If authoritativeness extent is below the treshold, don't bother calculating the matching probability
+			if ( authoritativenessExtent.compareTo( authoritativenessExtentTreshold ) >= 0  ) {
+				matchingProbability = calculateMatchingProbability();
+				tresholdReached = matchingProbability >= matchingProbabilityTreshold;
+			}
+			else {
+				matchingProbability = 0.0f;
+				tresholdReached     = false;
+			}
 		}
+		
 		@Override
 		public String toString() {
 			return String.format( "%-9s %3.1f%% - %s, %s  (%s, %s)", authoritativenessExtent.toString(), matchingProbability, analysis1.playerName, analysis2.playerName, analysis1.replay.getAbsolutePath(), analysis2.replay.getAbsolutePath() );
 		}
+		
 		public int compareTo( final Comparision comparision ) {
 			if ( authoritativenessExtent == comparision.authoritativenessExtent )
 				return -Float.compare( matchingProbability, comparision.matchingProbability );
-			return authoritativenessExtent.ordinal() < comparision.authoritativenessExtent.ordinal() ? -1 : 1;
+			return authoritativenessExtent.ordinal() < comparision.authoritativenessExtent.ordinal() ? 1 : -1;
+		}
+		
+		private AuthoritativenessExtent determineAuthoritativenessExtent() {
+			float authroitativenessExtent = 1.0f;
+			
+			// Comparing different races are a huge setback (the mathing algorithm uses action distribution pairing which changes with races)
+			if ( analysis1.race != analysis2.race )
+				authroitativenessExtent *= 0.5f;
+			
+			// Difference in APM is an obvious negative factor
+			if ( analysis1.realApm < 10 || analysis2.realApm < 10 )
+				authroitativenessExtent = 0.0f;
+			else
+				authroitativenessExtent *= analysis1.realApm < analysis2.realApm ? (float) analysis1.realApm / analysis2.realApm : (float) analysis2.realApm / analysis1.realApm;
+			
+			// If games differ in duration, the action distribution might change drastically (gather and micro focused on early, macro later on)
+			authroitativenessExtent *= analysis1.frames < analysis2.frames ? (float) analysis1.frames / analysis2.frames : (float) analysis2.frames / analysis1.frames;
+			
+			// Playing style changes over time. Replays from different ages are not very authoritative.
+			final int days  = (int) ( Math.abs( analysis1.replayDate - analysis2.replayDate ) / (1000l*60l*60l*24l) );
+			authroitativenessExtent *= days < 2000 ? ( 2000.0f - days ) / 2000.0f : 0.0f;
+			
+			// Safety checking (might be out of range due to rounding problems or if it remains 1.0):
+			final int extentOrdinal = (int) ( authroitativenessExtent * AuthoritativenessExtent.values().length );
+			if ( extentOrdinal > AuthoritativenessExtent.values().length - 1 )
+				return AuthoritativenessExtent.USELESS;
+			if ( extentOrdinal < 0 )
+				return AuthoritativenessExtent.EXCELLENT;
+			return AuthoritativenessExtent.values()[ extentOrdinal ];
+		}
+		
+		private float calculateMatchingProbability() {
+			return (float) Math.random() * 100.0f;
 		}
 	}
 	
@@ -126,6 +174,10 @@ public class AkaFinderTab extends ProgressLoggedTab {
 	 */
 	public AkaFinderTab() {
 		super( "AKA finder", IconResourceManager.ICON_AKA_FINDER, LOG_FILE_NAME );
+		
+		final List< AuthoritativenessExtent > authoritativenessExtentList = Arrays.asList( AuthoritativenessExtent.values() );
+		Collections.reverse( authoritativenessExtentList );
+		authoritativenessThresholdComboBox = new JComboBox( authoritativenessExtentList.toArray( new AuthoritativenessExtent[ authoritativenessExtentList.size() ] ) );
 		
 		authoritativenessThresholdComboBox  .setSelectedIndex( Integer.parseInt( Utils.settingsProperties.getProperty( Consts.PROPERTY_AUTHORITATIVENESS_TRESHOLD ) ) );
 		matchingProbabilityThresholdComboBox.setSelectedIndex( Integer.parseInt( Utils.settingsProperties.getProperty( Consts.PROPERTY_MATCHING_PROBABILITY_TRESHOLD ) ) );
@@ -251,14 +303,14 @@ public class AkaFinderTab extends ProgressLoggedTab {
 								
 								final int frames  = playerActions.actions.length > 0 ? playerActions.actions[ playerActions.actions.length - 1 ].iteration : 0;
 								final int seconds = ReplayHeader.convertFramesToSeconds( frames );
-								final PlayerAnalysis playerAnalysis = new PlayerAnalysis( replayFile, replayHeader.saveTime, playerActions.playerName, replayHeader.playerRaces[ playerIndex ],
+								final PlayerAnalysis playerAnalysis = new PlayerAnalysis( replayFile, replayHeader.saveTime.getTime(), playerActions.playerName, replayHeader.playerRaces[ playerIndex ],
 										frames, frames == 0 ? 0 : seconds == 0 ? 0 : playerActions.actions.length * 60 / seconds );
 								replayPlayerAnalysisList.add( playerAnalysis );
 								
 								for ( final PlayerAnalysis playerAnalysis2 : playerAnalysisList )
 									if ( compareSameNames || !playerAnalysis2.playerName.equals( playerAnalysis.playerName ) ) {
-										final Comparision comparision = new Comparision( playerAnalysis2, playerAnalysis );
-										if ( comparision.matchingProbability >= matchingProbabilityThreshold && comparision.authoritativenessExtent.compareTo( authoritativenessThreshold ) <= 0 )
+										final Comparision comparision = new Comparision( playerAnalysis2, playerAnalysis, authoritativenessThreshold, matchingProbabilityThreshold );
+										if ( comparision.tresholdReached )
 											comparisionList.add( comparision );
 									}
 							}
