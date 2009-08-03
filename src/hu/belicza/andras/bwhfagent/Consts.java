9bd0479fbc7ae38ed65e25ed8f14f473ccfb57a1
iczaaa <icza@users.noreply.github.com>
2009-08-03 22:22:57 +0000

    Syntax corrections.

--- src/hu/belicza/andras/bwhfagent/Consts.java
+++ src/hu/belicza/andras/bwhfagent/Consts.java
@@ -181,10 +181,10 @@ public class Consts {
 	public static final String PROPERTY_AUTO_SEND_INFO_ABOUT_LAST_REP  = "autoSendInfoAboutLastRep";
 	/** Don't compare same names property.       */
 	public static final String PROPERTY_DONT_COMPARE_SAME_NAMES        = "dontCompareSameNames";
-	/** Authoritativeness treshold property.     */
-	public static final String PROPERTY_AUTHORITATIVENESS_TRESHOLD     = "authoritativenessTreshold";
-	/** Matching probability treshold property.  */
-	public static final String PROPERTY_MATCHING_PROBABILITY_TRESHOLD  = "matchingProbabilityTreshold";
+	/** Authoritativeness threshold property.    */
+	public static final String PROPERTY_AUTHORITATIVENESS_THRESHOLD     = "authoritativenessThreshold";
+	/** Matching probability threshold property. */
+	public static final String PROPERTY_MATCHING_PROBABILITY_THRESHOLD  = "matchingProbabilityThreshold";
 	
 	/** Properties holding the default settings. */
 	public static final Properties DEFAULT_SETTINGS_PROPERTIES = new Properties();
@@ -246,8 +246,8 @@ public class Consts {
 		DEFAULT_SETTINGS_PROPERTIES.setProperty( PROPERTY_REPLAY_COLUMN_MODEL_INDICES   , "" );
 		DEFAULT_SETTINGS_PROPERTIES.setProperty( PROPERTY_AUTO_SEND_INFO_ABOUT_LAST_REP , "true" );
 		DEFAULT_SETTINGS_PROPERTIES.setProperty( PROPERTY_DONT_COMPARE_SAME_NAMES       , "true" );
-		DEFAULT_SETTINGS_PROPERTIES.setProperty( PROPERTY_AUTHORITATIVENESS_TRESHOLD    , "3" );
-		DEFAULT_SETTINGS_PROPERTIES.setProperty( PROPERTY_MATCHING_PROBABILITY_TRESHOLD , "2" );
+		DEFAULT_SETTINGS_PROPERTIES.setProperty( PROPERTY_AUTHORITATIVENESS_THRESHOLD    , "3" );
+		DEFAULT_SETTINGS_PROPERTIES.setProperty( PROPERTY_MATCHING_PROBABILITY_THRESHOLD , "2" );
 		
 	}
 	
